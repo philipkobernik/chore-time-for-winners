7631548215349d957c8904ffd46e08d4c7045d4b
Philip Kobernik <ptk921@gmail.com>
2012-12-10 20:14:16 -0800

    This is the beginning of greatness

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoreTime::Application.config.secret_token = '383ca7e209391a35e2da1f82deb4614777c4bf83af48e8018dc0d49781699627cea2f712ee1f977c0fc9d24d5189e6d680073720222c03925fa00461c9b1b42d'
